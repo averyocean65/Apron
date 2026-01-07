3d4b0d15060d0677f6d83643f47e8bea7b5ac2ad
averyocean65 <averytoad65@gmail.com>
2026-01-07 21:00:47 +0100

    add sample instructions to runtime main.cpp

--- Runtime/main.cpp
+++ Runtime/main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 #include <apron/core/init.hpp>
+#include <apron/instructions/instruction.hpp>
+#include <apron/instructions/instruction_type.h>
+
+using namespace apron::instructions;
 
 int main(int argc, char** argv) {
-	std::cout << "Hello, World!\n";
-	std::cout << "Apron Init: " << init_library() << "\n";
+    if (apron_shared_init() != APRON_SUCCESS)
+    {
+        std::cerr << "Failed to initialize Apron Shared library!\n";
+        return 1;
+    }
+    
+    instruction instructions[] = {
+        // Write 0x12345678 to register 1
+        instruction(instruction_type::MEMORY_WRITE, 0x1, 0x12345678),
+
+        // Copy the data from register 1 into register 2
+        instruction(instruction_type::MEMORY_COPY, 0x2, 0x1),
+
+        // Multiply registers 2 and 1 together into register 3
+        instruction(instruction_type::ARITHMETIC_MUL, 0x3, 0x00020001)
+    };
 }
