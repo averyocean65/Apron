2f37150fe13b477d715d4034b394829ae717db83
averyocean65 <averytoad65@gmail.com>
2026-01-08 10:28:55 +0100

    implement and rework cpu parts

--- Runtime/devices/cpu.cpp
+++ Runtime/devices/cpu.cpp
@@ -1,1 +1,25 @@
 ﻿#include "cpu.h"
+
+cpu::cpu(int register_count)
+    : m_register_count(register_count)
+{
+    m_registers = std::vector<uint32_t>(register_count);
+}
+
+void cpu::execute_instruction(apron::instructions::instruction instruction)
+{
+    printf("Running instruction: %#04X %#04X %#08X", instruction.id, instruction.additional, instruction.data);
+
+    // TODO
+}
+
+uint32_t* cpu::get_register(int index)
+{
+    if (index < 0 || index >= m_register_count)
+    {
+        return nullptr;
+    }
+
+    return &m_registers[index];
+}
+
--- Runtime/devices/cpu.h
+++ Runtime/devices/cpu.h
@@ -1,13 +1,19 @@
 ﻿#pragma once
 #include <cstdint>
+#include <vector>
+
+#include "apron/instructions/instruction.hpp"
 
 class cpu
 {
 private:
-    uint32_t mRegisters[];
+    std::vector<uint32_t> m_registers;
+
+    // not required, just for ease of use
+    int m_register_count;
 public:
     cpu(int register_count);
     
-    void execute_instruction();
+    void execute_instruction(apron::instructions::instruction instruction);
     uint32_t* get_register(int index);
 };
