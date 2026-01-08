ef26307ade3a2276e09428f2a4c47ca14eaec8fc
averyocean65 <averytoad65@gmail.com>
2026-01-08 10:05:13 +0100

    added int-splitters

---
+++ Shared/apron/utils/splitter.cpp
@@ -0,0 +1,15 @@
+﻿#include "splitter.h"
+
+APRON_API void apron::utils::split_u16(uint16_t base, uint8_t* a, uint8_t* b)
+{
+    *a = base >> 8;
+    *b = base;
+}
+
+APRON_API void apron::utils::split_u32(uint32_t base, uint8_t* a, uint8_t* b, uint8_t* c, uint8_t* d)
+{
+    *a = base >> 24;
+    *b = base >> 16;
+    *c = base >> 8;
+    *d = base >> 0;
+}
---
+++ Shared/apron/utils/splitter.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include <cstdint>
+#include "../core/core.hpp"
+
+namespace apron
+{
+    namespace utils
+    {
+        APRON_API void split_u16(uint16_t base, uint8_t* a, uint8_t* b);
+        APRON_API void split_u32(uint32_t base, uint8_t* a, uint8_t* b, uint8_t* c, uint8_t *d);
+    }
+}
