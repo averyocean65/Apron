cfce169a665ecdd736aa0841b3dee538315bf0bd
averyocean65 <averytoad65@gmail.com>
2026-01-08 10:04:43 +0100

    unfinished basic cpu framework

---
+++ Runtime/devices/cpu.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include <cstdint>
+
+class cpu
+{
+private:
+    uint32_t mRegisters[];
+public:
+    cpu(int register_count);
+    
+    void execute_instruction();
+    uint32_t* get_register(int index);
+};
