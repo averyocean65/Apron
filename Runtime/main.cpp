6b1e3a45099986d562a4c6742f813211b6fcdf0c
averyocean65 <averytoad65@gmail.com>
2026-01-08 10:05:31 +0100

    update build events + splitter tests

--- Runtime/main.cpp
+++ Runtime/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <apron/core/init.hpp>
+
 #include <apron/instructions/instruction.hpp>
 #include <apron/instructions/instruction_type.h>
+
+#include <apron/utils/splitter.h>
 
 using namespace apron::instructions;
 
@@ -12,14 +15,17 @@ int main(int argc, char** argv) {
         return 1;
     }
     
-    instruction instructions[] = {
-        // Write 0x12345678 to register 1
-        instruction(instruction_type::MEMORY_WRITE, 0x1, 0x12345678),
+    uint16_t baseA = 0x1234;
+    uint32_t baseB = 0x12345678;
+    
+    uint8_t a = 0;
+    uint8_t b = 0;
+    uint8_t c = 0;
+    uint8_t d = 0;
 
-        // Copy the data from register 1 into register 2
-        instruction(instruction_type::MEMORY_COPY, 0x2, 0x1),
-
-        // Multiply registers 2 and 1 together into register 3
-        instruction(instruction_type::ARITHMETIC_MUL, 0x3, 0x00020001)
-    };
+    apron::utils::split_u16(baseA, &a, &b);
+    printf("uint16_t split: %02X, %02X\n", a, b);
+    
+    apron::utils::split_u32(baseB, &a, &b, &c, &d);
+    printf("uint32_t split: %02X, %02X, %02X, %02X\n", a, b, c, d);
 }
--- Shared/apron/instructions/instruction_type.h
+++ Shared/apron/instructions/instruction_type.h
@@ -3,11 +3,30 @@
 
 enum class instruction_type : uint16_t
 {
+    /**
+     * The specification for all of these functions is:
+     * <instruction_id> <source, destination> <write_data>
+     *
+     * for example, writing 0x1234 to register 1 would be:
+     * MEMORY_WRITE 0x0100 0x1234
+     * Note: the <source, destination> turns into <register 1, register 2>
+     *
+     * copying from register 2 to register 5 would be:
+     * MEMORY_COPY 0x0502 0x0
+     */
+    
     MEMORY_COPY             = 0x0000,
     MEMORY_MOVE             = 0x0001,
     MEMORY_WRITE            = 0x0002,
-    MEMORY_READ             = 0x0003,
-    MEMORY_CLEAR            = 0x0004,
+    MEMORY_CLEAR            = 0x0003,
+
+    /**
+     * The specification for all of these functions is:
+     * <instruction_id> <destination, register 1> <register 2, register 3, register 4, register 5>
+     *
+     * for example, multiplying registers 5, 3, 1 and 9 into register 2 would be:
+     * ARITHMETIC_MUL 0x0205 0x03010902
+     */
 
     ARITHMETIC_ADD          = 0x0100,
     ARITHMETIC_SUB          = 0x0101,
