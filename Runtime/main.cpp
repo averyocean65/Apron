c15162419cbb3a3e0dd3d3bc939857183f7a7ee8
averyocean65 <averytoad65@gmail.com>
2026-01-09 20:16:48 +0100

    undo my rename of init.hpp to init.h

--- Runtime/main.cpp
+++ Runtime/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-#include <apron/core/init.h>
+#include <apron/core/init.hpp>
 #include <apron/instructions/instruction.hpp>
 #include <apron/utils/splitter.hpp>
 
--- Shared/apron/core/init.cpp
+++ Shared/apron/core/init.cpp
@@ -1,4 +1,4 @@
-#include "init.h"
+#include "init.hpp"
 
 APRON_API int apron_shared_init() {
 	// let the user know that the library is up and running!
